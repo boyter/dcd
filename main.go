74b65b6115a4c523afcc7c9a8ea0244dcc238273
Ben Boyter <ben@boyter.org>
2020-09-24 20:24:07 +1000

    tweak

--- main.go
+++ main.go
@@ -162,34 +162,43 @@ func main() {
 	for key, files := range extensionFileMap {
 		fmt.Println(key)
 
-		// Loop the files in total
+		// Loop all of the files for this extension
 		for i := 0; i < len(files); i++ {
 			fmt.Println(files[i].Location)
 
 			// Loop against surrounding files
 			for j := i; j < len(files); j++ {
-				//don't compare to itself this way, if the same file we need to instead
+				// don't compare to itself this way, if the same file we need to instead
 				// compare but only lines which are not the same
 				if i != j {
 
 					var sb strings.Builder
 					// at this point loop this files lines, looking for matching lines in the other file
 
+					var outer [][]bool
 					for _, line := range files[i].Lines {
+						var inner []bool
 						for _, line2 := range files[j].Lines {
 							if line == line2 {
 								sb.WriteString("1")
+								inner = append(inner, true)
 							} else {
 								sb.WriteString("0")
+								inner = append(inner, false)
 							}
 						}
+
+						outer = append(outer, inner)
 						sb.WriteString("\n")
 					}
 
-					_ = ioutil.WriteFile(fmt.Sprintf("%s_%s.pbm", files[i].Filename, files[j].Filename), []byte(fmt.Sprintf(`P1
-# Matches...
-%d %d
-%s`, len(files[j].Lines), len(files[i].Lines), sb.String())), 0600)
+					// now we need to check if there are any duplicates in there....
+					identifyDuplicates(outer)
+
+										_ = ioutil.WriteFile(fmt.Sprintf("%s_%s.pbm", files[i].Filename, files[j].Filename), []byte(fmt.Sprintf(`P1
+					# Matches...
+					%d %d
+					%s`, len(files[j].Lines), len(files[i].Lines), sb.String())), 0600)
 
 				}
 			}
@@ -198,8 +207,35 @@ func main() {
 
 	}
 
-
-
 	t := str.IndexAllIgnoreCase("", "", -1)
 	fmt.Println(t)
+}
+
+// Duplicates consist of diagonal matches so
+//
+// 1 0 0
+// 0 1 0
+// 0 0 1
+//
+// If 1 were considered a match then the 3 diagonally indicate
+// some copied code. The algorithm to check this is to look for any
+// positive match, then if found check to the right
+func identifyDuplicates(outer [][]bool) {
+	for i := 0; i< len(outer); i++ {
+		for j := 0; j < len(outer[i]); j++ {
+			if outer[i][j] {
+				for k := 1; k < len(outer); k++ {
+					if outer[i+k][j+k] {
+						fmt.Println(i+k, j+k)
+					} else {
+						// if its not a match anymore, break
+						break
+					}
+				}
+
+				// now that we have found a match, walk to the right and down to see if there is a match
+				// from this position start walking down and to the right to see how long a match we can find
+			}
+		}
+	}
 }
